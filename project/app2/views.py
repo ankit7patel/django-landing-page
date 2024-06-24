2e09bb6dfe35d37e82f58be8d6469ec875ce83a4
ankittt9 <ankitptl7024@gmail.com>
2024-06-24 22:00:49 +0530

    first

--- project/app2/views.py
+++ project/app2/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("welcome to my contact page........v3")
+    return HttpResponse("welcome to my contact page...........v3")
