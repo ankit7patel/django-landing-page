a3f59bd7be4ea2efd26c3a446e2040870a90581e
ankittt9 <ankitptl7024@gmail.com>
2024-06-24 21:50:12 +0530

    aap commit

--- project/project/views.py
+++ project/project/views.py
@@ -1,10 +1,15 @@
-# from django.http import HttpResponse
+# from django.http import HttpResponse,
 
 # def index(request):
 #     return HttpResponse("welcome to landing 1 page")
 
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
 
 def index(request):
     return HttpResponse("<h5>welcome to my landing page</h5>")
+    
+
+def index(request):
+    return HttpResponse("welcome to my landing page")
